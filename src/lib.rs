222f004186701b5ac45d00d5d479d13ba84d160a
ha-spring <spring4boot@gmail.com>
2023-06-07 09:50:14 +0000

    Writing and Testing a Function to Count File Elements

--- src/lib.rs
+++ src/lib.rs
@@ -14,11 +14,33 @@ pub struct Config {
     chars: bool,
 }
 
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+  num_lines: usize,
+  num_words: usize,
+  num_bytes: usize,
+  num_chars: usize,
+}
+
 fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
         _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
     }
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+  let mut num_lines = 0;
+  let mut num_words = 0;
+  let mut num_bytes = 0;
+  let mut num_chars = 0;
+
+  Ok(FileInfo {
+    num_lines,
+    num_words,
+    num_bytes,
+    num_chars,
+  })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
@@ -91,4 +113,24 @@ pub fn get_args() -> MyResult<Config> {
         bytes,
         chars,
     })
+}
+
+#[cfg(test)]
+mod tests {
+  use super::{count, FileInfo};
+  use std::io::Cursor;
+
+  #[test]
+  fn test_count() {
+    let text = "I don't want the world. I just want your half.\r\n";
+    let info = count(Cursor::new(text));
+    assert!(info.is_ok());
+    let expected = FileInfo {
+      num_lines: 1,
+      num_words: 10,
+      num_chars: 48,
+      num_bytes: 48,
+    };
+    assert_eq!(info.unwrap(), expected);
+  }
 }
